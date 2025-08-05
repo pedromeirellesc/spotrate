096aa274dfd0b8f9c4a33e05c064481f9974a0b5
= <pedromeirellescarrion@gmail.com>
2025-08-04 23:10:57 -0300

    wip

--- resources/views/components/register-modal.blade.php
+++ resources/views/components/register-modal.blade.php
@@ -35,7 +35,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-secondary hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+            <a class="underline text-sm text-secondary hover hover:text-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:text-gray-400"
                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'host.docker.internal',
+            port: 5173,
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
